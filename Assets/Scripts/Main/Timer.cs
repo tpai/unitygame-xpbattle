699f224878e909a15cd4742af915b0693b9d2623
Tony Pai <tony77794@gmail.com>
2019-12-22 01:15:08 +0800

    Implement Win95 theme level

--- Assets/Scripts/Main/Timer.cs
+++ Assets/Scripts/Main/Timer.cs
@@ -1,9 +1,9 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour {
-
+	public string shutDownScene = Scene.Shutdown;
 	public float maxTime = 30f;
 
 	private bool m_countdown = false;
@@ -25,7 +25,7 @@ public class Timer : MonoBehaviour {
 			m_nowTime -= Time.deltaTime;
 
 		if (m_nowTime <= 0)
-			Application.LoadLevel ("Shutdown");
+			SceneManager.LoadScene(shutDownScene);
 
 		GetComponent<Image>().fillAmount = m_nowTime / maxTime;
 	}
---
+++ Assets/Scripts/Win95/ShutdownTextWin95.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ShutdownTextWin95 : MonoBehaviour {
+
+	Text shutdownText;
+
+	void Start () {
+		shutdownText = GetComponent<Text> ();
+
+		string contentTW = "謝謝你，老朋友";
+		string contentEN = "Thank you, old friend.";
+		string content;
+		if (
+			Application.systemLanguage == SystemLanguage.Chinese ||
+			Application.systemLanguage == SystemLanguage.ChineseTraditional || 
+			Application.systemLanguage == SystemLanguage.ChineseSimplified
+		)
+			content = contentTW;
+		else
+			content = contentEN;
+		shutdownText.text = content;
+
+		int passed = PlayerPrefs.GetInt ("Passed");
+		PlayerPrefs.SetInt ("Passed", passed + 1);
+#if UNITY_WEBGL || UNITY_WEBPLAYER
+		GJBase.instance.UnlockRetroTrophy();
+#endif
+	}
+}
